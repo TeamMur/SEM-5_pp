f7ae2854d60e53cc31cd157d3d04eb1e5bdbbee5
TeamMur <152087842+TeamMur@users.noreply.github.com>
2024-09-16 10:01:23 +0400

    lab_1 update
    добавлено множество ненужностей
    но они скорее нужны при сдаче

--- lab_1/main.go
+++ lab_1/main.go
@@ -19,32 +19,54 @@ func main() {
 	fmt.Printf("\n2-3. Переменные различных типов:\nint %v\nfloat64 %v\nstring %v\nbool %v\n\n", varInt, varFloat64, varString, varBool)
 
 	//Написать программу для выполнения арифметических операций с двумя целыми числами и выводом результатов
-	num1 := 2
-	num2 := 3
+	var userInt, userInt2 int
 	fmt.Println("4. Арифметические операции с двумя целыми числами:")
-	fmt.Println("Сумма ", num1+num2)
-	fmt.Println("Разность ", num1-num2)
-	fmt.Println("Произведение ", num1*num2)
+	fmt.Print("Введите число 1: ")
+	fmt.Scan(&userInt)
+	fmt.Print("Введите число 2: ")
+	fmt.Scan(&userInt2)
+
+	sum := userInt + userInt2
+	difference := userInt - userInt2
+	multiply := userInt * userInt2
+	var divide float64 = float64(userInt) / float64(userInt2)
+
+	fmt.Println("Сумма ", sum)
+	fmt.Println("Разность ", difference)
+	fmt.Println("Произведение ", multiply)
 	fmt.Print("Частное ")
-	if num2 != 0 {
-		fmt.Print(num1 / num2)
+	if userInt2 != 0 {
+		fmt.Print(divide)
 	} else {
-		fmt.Print("ошибка")
+		fmt.Print("ошибка - деление на ноль")
 	}
 	fmt.Println("\n")
 
 	//Реализовать функцию для вычисления суммы и разности двух чисел с плавающей запятой
-	floatOperations(1.2, 4.3)
+	floatOperations()
 
 	//Написать программу, которая вычисляет среднее значение трех чисел
-	avNum1 := 2
-	avNum2 := 4.23
-	avNum3 := 5.3
-	fmt.Printf("Среднее значение трёх чисел %0.2f\n", (float64(avNum1)+avNum2+avNum3)/3)
+	fmt.Println("6. Среднее значение 3х чисел:")
+	var userFloat, userFloat2, userFloat3 float64
+	fmt.Print("Введите число 1: ")
+	fmt.Scan(&userFloat)
+	fmt.Print("Введите число 2: ")
+	fmt.Scan(&userFloat2)
+	fmt.Print("Введите число 3: ")
+	fmt.Scan(&userFloat3)
+	var resultSix = (userFloat + userFloat2 + userFloat3) / 3
+	fmt.Printf("Среднее значение трёх чисел %0.2f\n", resultSix)
 }
 
-func floatOperations(a float32, b float32) {
-	fmt.Println("Операция над числами с плавающей запятой:")
-	fmt.Printf("Сумма %0.2f\n", a+b)
-	fmt.Printf("Разность %0.2f\n\n", a-b)
+func floatOperations() {
+	fmt.Println("5. Операция над числами с плавающей запятой:")
+	var userA, userB float64
+	fmt.Print("Введите число 1: ")
+	fmt.Scan(&userA)
+	fmt.Print("Введите число 2: \n")
+	fmt.Scan(&userB)
+	sum := userA + userB
+	difference := userA - userB
+	fmt.Printf("Сумма %0.2f\n", sum)
+	fmt.Printf("Разность %0.2f\n\n", difference)
 }
